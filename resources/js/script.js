f1c28d1d2c0e810c9ce0a6884eb80ca950f1d109
ittus <vuminhthang.cm@gmail.com>
2016-03-28 02:00:26 +0800

    add scroll animation to anchor point

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
     
    
   
-    
+    /* Sticky notification */  
     var waypoint = new Waypoint({
-      element: document.getElementById('js--section-features'),
+      element: document.getElementById('features'),
       handler: function(direction) {
         if (direction == "down"){
             $('nav').addClass('sticky');
@@ -16,5 +16,27 @@ $(document).ready(function(){
         }
       },
       offset: '60'
+    });
+//    js--section-features
+    $('.js--scrol-to-plan').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+    });
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+    });
+    
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+        });
     });
 });
